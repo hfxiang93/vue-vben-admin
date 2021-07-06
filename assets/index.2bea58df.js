7fd75a5dc61bf4078d8821d6d3e814681899325b
xianghaifeng <850442777@qq.com>
2021-07-06 11:46:43 +0800

    Updates

--- assets/index.2bea58df.js
+++
@@ -1,1 +0,0 @@
-import{ak as r,ac as s,w as t}from"./index.49a9f596.js";import{k as a}from"./vendor.5450320c.js";const o=t(a({name:"Authority",props:{value:{type:[Number,Array,String],default:""}},setup(t,{slots:a}){const{hasPermission:o}=r();return()=>function(){const{value:r}=t;return r?o(r)?s(a):null:s(a)}()}}));export{o as A};
--- assets/upload.f4eacfd1.js
+++
@@ -1,1 +0,0 @@
-import{u as o,aw as r}from"./index.49a9f596.js";const{uploadUrl:a=""}=o();function s(o,s){return r.uploadFile({url:a,onUploadProgress:s},o)}export{s as u};
